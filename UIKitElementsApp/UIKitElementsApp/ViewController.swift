ec1d6af1910e81f4448d5f672522ecc254898965
Martynov Evgeny <martynov@wimix.by>
2022-10-18 20:11:57 +0300

    New project created

---
+++ UIKitElementsApp/UIKitElementsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitElementsApp
+//
+//  Created by Martynov Evgeny on 18.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
